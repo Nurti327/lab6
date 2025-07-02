cf00d178bccbd2ce14a54bdb2eaae5e002a67202
Nurti327 <nurtilekmanatbay@gmail.com>
2025-07-02 12:48:26 +0500

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> arr = {4, 1, 2, 4, 6, 4, 3};
+    int target = 4;
+    int freq = count(arr.begin(), arr.end(), target);
+    int minVal = *min_element(arr.begin(), arr.end());
+    int maxVal = *max_element(arr.begin(), arr.end());
+    cout << "Count of " << target << ": " << freq << endl;
+    cout << "Min: " << minVal << endl;
+    cout << "Max: " << maxVal << endl;
+    return 0;
+}
